98bde6e534ddfcd9c375e0e36f27614c402b11bd
Dmitry <40324321+inilim@users.noreply.github.com>
2024-09-16 23:37:03 +0300

    ref

--- src/LazyMethodAbstract.php
+++ src/LazyMethodAbstract.php
@@ -13,27 +13,27 @@ abstract class LazyMethodAbstract
     /**
      * @internal desc
      * @param string $name
-     * @param mixed[]|array{} $arguments
+     * @param mixed[]|array{} $args
      * @return mixed|void
      */
-    function __call($name, $arguments)
+    function __call($name, $args)
     {
-        return self::__callStatic($name, $arguments);
+        return self::__callStatic($name, $args);
     }
 
     /**
      * @internal desc
      * @param string $name
-     * @param mixed[]|array{} $arguments
+     * @param mixed[]|array{} $args
      * @return mixed|void
      */
-    static function __callStatic($name, $arguments)
+    static function __callStatic($name, $args)
     {
         $n = static::ALIAS[$name] ?? $name;
         $fn = static::NAMESPACE . '\\' . $n;
 
         if (\str_contains(self::$exists . '|', '|' . $n . '|')) {
-            return $fn(...$arguments);
+            return $fn(...$args);
         }
 
         $file = static::PATH_TO_DIR . '/' . $n . '.php';
@@ -43,7 +43,7 @@ abstract class LazyMethodAbstract
 
             if (\function_exists($fn)) {
                 self::$exists .= '|' . $n;
-                return $fn(...$arguments);
+                return $fn(...$args);
             }
         }
 
